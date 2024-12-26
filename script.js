74dd292e09997f1152cd97b45d6a0eed422c5d28
Jimmy <james117718@gmail.com>
2024-12-26 22:20:16 +0200

    evenets change big

--- script.js
+++ script.js
@@ -100,3 +100,44 @@ document.querySelector('.scroll-to-top').addEventListener('click', function (eve
   // Scroll smoothly to the next section
   document.querySelector('#top').scrollIntoView({ behavior: 'smooth' });
 });
+
+
+
+const next = document.querySelector(".next");
+const prev = document.querySelector(".previous");
+
+const numImg = document.querySelectorAll("img").length;
+let currImg = 1;
+
+let timeoutID;
+
+next.addEventListener("click", () => {
+  currImg++;
+  clearTimeout(timeoutID);
+  updateImage();
+});
+
+prev.addEventListener("click", () => {
+  currImg--;
+  clearTimeout(timeoutID);
+
+  updateImage();
+});
+
+const imgcontainer = document.querySelector(".img-container");
+
+function updateImage() {
+  if (currImg > numImg) {
+    currImg = 1;
+  } else if (currImg < 1) {
+    currImg = numImg;
+  }
+  imgcontainer.style.transform = `translateX(-${(currImg - 1) * 800}px)`;
+
+  timeoutID = setTimeout(() => {
+    currImg++;
+    updateImage();
+  }, 2000);
+}
+
+updateImage();
